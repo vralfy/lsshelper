e8be2e4d32dbaa5a7cebec2ded092f3a705be4bf
Norman Specht <webmaster@foopara.de>
2025-04-10 16:39:11 +0200

    Enhance mission handling and vehicle management
    - Added new vehicle types for improved vehicle list display.
    - Introduced mission scenes for better mission categorization.
    - Implemented a new function to print missions with state-based filtering.
    - Updated vehicle list sorting based on average credits.
    - Improved styling for mission states in the UI.

--- lsshelper.user.js
+++ lsshelper.user.js
@@ -25,9 +25,24 @@
           '28': '🚑 RTW',
           '32': '🚓 FuStW',
           '37': '🚒 TSF-W',
+          '121': '🚒 GT-LF',
+          '166': '🚒 PT-LF',
       },
       vehicles: [],
       missions: [],
+      scenes: {
+          'X' : { '0': 2 },
+          'lf1' : { '0': 1 },
+          '5' : { '0': 2, '2': 1, '3': 1, '32': 1}, // Zimmerbrand
+          '9' : { '0': 1 }, // Strohballenbrand
+          '161' : { '0': 1 }, // Baum auf PKW
+          '240' : { '0': 3, '3': 1, '28': 1, '32': 1}, // Gasgeruch
+          '282' : { '0': 2 }, // Geplatzte Wasserleitung
+          '476' : { '28': 1, '32': 1}, // Angefahrener Radfahrer
+          '518' : { '0': 1 }, // Brennender Baum
+          '607' : { '32': 1}, // Verkehrserziehung an Schule
+          '733' : { '0': 2 }, // Absicherung Osterfeuer
+      },
   };
 
   document.lss_helper.init = () => {
@@ -40,6 +55,9 @@
           ".lss_available { color: #0a0; }",
           ".lss_unavailable { color: #a00; }",
           ".lss_call { color: '#f00'; }",
+          ".state_finishing { color: #000; background: #0f0 }",
+          ".state_unattended { color: #000; background: #f00 }",
+          ".state_attended { color: #000; background: #ff0 }",
           "#mission_general_info, #back_to_mission { text-align:right }",
           "</style>"
       ].join("\n")).appendTo("head");
@@ -51,16 +69,19 @@
   document.lss_helper.update = () => {
       document.lss_helper.updateLists();
       document.lss_helper.printVehicleList();
+      document.lss_helper.printMissions();
       setTimeout(() => {document.lss_helper.update();}, document.lss_helper.updateInterval);
   };
 
   document.lss_helper.updateLists = () => {
+      document.lss_helper.authToken = Array.from(document.getElementsByName('csrf-token'))[0].content;
       document.lss_helper.vehicles = Array.from(document.getElementById('building_list').getElementsByClassName('building_list_vehicle_element'))
       .map((vehicle) => {
           const img = vehicle.getElementsByTagName('img')[0];
           const status = vehicle.getElementsByTagName('span')[0];
           const link = vehicle.getElementsByTagName('a')[0];
           return {
+              id: parseInt(vehicle.attributes.vehicle_id.value.trim()),
               status: status.innerHTML.trim(),
               type: link.attributes.vehicle_type_id.value.trim(),
               name: link.innerHTML.trim(),
@@ -84,7 +105,8 @@
               missionType: m.attributes['mission_type_id'].value.trim(),
               origin: m,
           }
-      });
+      })
+      .sort((m1, m2) => m1.data.average_credits - m2.data.average_credits);
   };
 
   document.lss_helper.getHelperContainer = () => {
@@ -135,11 +157,11 @@
       }
       containerAvailable = document.createElement("ul");
       containerAvailable.id = 'lss_helper_vehicle_available';
-      containerAvailable.classList = 'col-md-4';
+      containerAvailable.classList = 'col-md-3';
       main.appendChild(containerAvailable);
       containerUnavailable = document.createElement("ul");
       containerUnavailable.id = 'lss_helper_vehicle_unavailable';
-      containerUnavailable.classList = 'col-md-4';
+      containerUnavailable.classList = 'col-md-3';
       main.appendChild(containerUnavailable);
 
       const itemsAvailable = {};
@@ -179,9 +201,86 @@
           li.innerHTML = i.length + ' ' + (document.lss_helper.vehicleTypes[i[0].type] || (i[0].type + ' - ' + i[0].name));
           containerUnavailable.append(li)
       });
-  }
+  };
 
+  document.lss_helper.printMissions = () => {
+      const main = document.lss_helper.getHelperContainer();
+      let missionsContainer = document.getElementById('lss_helper_missions');
+      if (missionsContainer) {
+          missionsContainer.remove();
+      }
+      missionsContainer = document.createElement("ul");
+      missionsContainer.id = 'lss_helper_missions';
+      missionsContainer.classList = 'col-md-6';
+      main.appendChild(missionsContainer);
 
+      document.lss_helper.missions
+      .filter((m) => m.state != 'finishing')
+      .sort((m1,m2) => m1.state < m2.state ? -1 : 1)
+      .forEach((m) => {
+          const li = document.createElement('li');
+          li.classList = 'state_' + m.state;
+          missionsContainer.appendChild(li);
+
+          const btn = document.createElement('a');
+          btn.classList= 'btn btn-xs btn-default';
+          btn.innerHTML = '🚒';
+          btn.onclick = () => {document.lss_helper.sendByScene(m, 'lf1')};
+
+          const btn2 = document.createElement('a');
+          btn2.classList= 'btn btn-xs btn-default';
+          btn2.innerHTML = document.lss_helper.scenes[m.missionType] ? '🚨' : '🚒🚒';
+          btn2.onclick = () => {document.lss_helper.sendByScene(m)};
+
+          const txt = document.createElement('span');
+          txt.innerHTML = m.data.caption;
+
+          if (m.state === 'unattended') {
+              li.appendChild(btn);
+              li.appendChild(btn2);
+          }
+          li.appendChild(txt);
+      });
+  };
+
+  document.lss_helper.sendByScene = (mission, scene) => {
+      scene = scene || (document.lss_helper.scenes[mission.missionType] ? mission.missionType : null) || 'X';
+      scene = document.lss_helper.scenes[scene];
+
+      console.warn('Sending LF:', mission.missionType, scene, mission);
+      const available = document.lss_helper.vehicles.filter((v) => v.available);
+      const vehicles = Object.keys(scene).map((vt) => {
+          const r = available.filter((v) => v.type === vt).slice(0, scene[vt]);
+          return r.length === scene[vt] ? r : null;
+      });
+      if (vehicles.filter((v) => v === null).length) {
+          console.warn('Not enough vehicles');
+      } else {
+          const v = vehicles.reduce((acc, cur) => [...acc, ...cur], []);
+          document.lss_helper.sendVehicles(mission.missionId, v);
+          document.lss_helper.updateLists();
+      }
+  };
+
+  document.lss_helper.sendVehicles = (missionid, vehicles) => {
+      const url = "/missions/" + missionid + "/alarm";
+      const body = {
+          //utf8: "",
+          authenticity_token: document.lss_helper.authToken,
+          commit: "Alarmieren",
+          next_mission: 0,
+          next_mission_id: 0,
+          alliance_mission_publish: 0,
+          sk: "ac",
+          sd: "d",
+          ifs: "fi",
+      };
+
+      const vehicleids = vehicles.map((v) => new URLSearchParams('vehicle_ids[]') + v.id).join('&');
+      fetch(url, {method: 'POST', body: new URLSearchParams(body) + '&' + vehicleids, headers: { "Content-type": "application/x-www-form-urlencoded; charset=UTF-8" }})
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+  };
 
   document.lss_helper.init();
   document.lss_helper.update();
