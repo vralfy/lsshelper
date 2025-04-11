05735da8a4c2e6c7a6d48bb0e197730d4dd738df
Norman Specht <webmaster@foopara.de>
2025-04-11 17:03:29 +0200

    Update incident handling and enhance data processing - Improved handling for various incident types to streamline data processing. - Refactored existing code for better maintainability and performance.

--- lsshelper.user.js
+++ lsshelper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Leistellenspiel Helper
 // @namespace    http://tampermonkey.net/
-// @version      202504-10-02
+// @version      202504-11-01
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.leitstellenspiel.de/
@@ -12,7 +12,7 @@
 (function() {
   'use strict';
   document.lss_helper = {
-      updateInterval: 1000,
+      storage: localStorage,
       vehicleTypes: {
           '0': '🚒 LF',
           '2': '🚨 DLK',
@@ -42,25 +42,75 @@
       scenes: {
           'X' : { '0': 2 },
           'lf1' : { '0': 1 },
+          '1' : { '0': 1, '32': 1 }, // Brennender Abfallcontainer
           '5' : { '0': 2, '2': 1, '3': 1, '32': 1}, // Zimmerbrand
+          '8' : { '0': 1, '32': 1}, // Sperrmüllbrand
           '9' : { '0': 1 }, // Strohballenbrand
+          '10' : { '0': 1 }, // Traktorbrand
+          '11' : { '0': 1 }, // Brennende Telefonzelle
+          '18' : { '0': 1 }, // Brennendes Gebüsch
           '26' : { '0': 3, '2': 1, '4': 1, '3': 1, '34': 1 }, // Brand im Supermarkt
+          '28' : { '0': 2 }, // Garagenbrand
+          '29' : { '0': 3, '3': 1, '4': 1, '5': 1 }, // Maschinenbrand
+          '30' : { '0': 1, '10': 1 }, // Große Ölspur
+          '31' : { '0': 1, '4': 1 }, // Auslaufende Betriebsstoffe
+          '32' : { '0': 2, '2': 1, '3': 1 }, // Kaminbrand
+          '44' : { '28': 1 }, // Alkoholintoxication
+          '57' : { '28': 1 }, // Gestürzter Fußgänger
+          '67' : { '32': 1}, // Personalienaufnahme nach Schwarzfahrer
+          '72' : { '0': 1, '28': 1, '32': 1, '33': 1 }, // Bewußtloser Kranführer
+          '78' : { '0': 1 }, // Kleintier in Not
           '79' : { '0': 1, '3': 1, '28': 1, '33': 1 }, // Verletzte Person auf Baugerüst
+          '82' : { '32': 1 }, // Einbruch in Wohnung
           '90' : { '32': 1}, // Ruhestörung
           '102' : { '0': 3, '2': 1, '3': 1 }, // Feuer in Einfamilienhaus
+          '124' : { '0': 1, '4': 1, '28': 2 }, // Verkehrsunfall
+          '126' : { '0': 2, '4': 1, '28': 3 }, // Verkehrsunfall
+          '128' : { '0': 1, '28': 1, '32': 1 }, // Auffahrunfall
+          '131' : { '0': 5, '3': 1, '11': 1}, // Mittlerer Feldbrand
+          '132' : { '0': 6, '3': 1, '11': 1}, // Großer Feldbrand
+          '138' : { '0': 2 }, // Flächenbrand
           '150' : { '32': 1}, // Hausfriedensbruch
           '153' : { '28': 1, '32': 1}, // Hilflose Person
           '161' : { '0': 1 }, // Baum auf PKW
+          '190' : { '32': 1 }, // Diebstahl aus Kfz
+          '230' : { '0': 1 }, // Feuerprobealarm an Schule
           '235' : { '0': 2, '3': 1, '28': 1, '32': 1, '33': 1}, // Verletzte Person auf Hochspannungsmast
-          '240' : { '0': 3, '3': 1, '28': 1, '32': 1}, // Gasgeruch
+          '240' : { '0': 3, '3': 1, '27': 1, '28': 1, '32': 1}, // Gasgeruch
           '282' : { '0': 2, '3': 1, '4': 1 }, // Geplatzte Wasserleitung
+          '284' : { '32': 1 }, // Ostereierdieb
+          '288' : { '0': 1 }, // Brennendes Osternest
+          '289' : { '10': 1 }, // Schokoladenspur auf Straße
+          '290' : { '0': 2 }, // Eierkocherbrand
+          '308' : { '0': 2 }, // Brennender Müllwagen
+          '318' : { '0': 1 }, // Ausgelöster Heimrauchmelder
+          '349' : { '0': 1 }, // Brandgeruch
+          '392' : { '0': 2, '3': 1, '4': 1, '28': 1 }, // Person durch Hubmaschine eingeklemmt
+          '436' : { '0': 1, '4': 1, '32': 1 }, // Verschmutzte Fahrbahn
+          '448' : { '32': 1 }, // Ermittlungen nach Unfallflucht
           '460' : { '0': 1, '2': 1, '3': 1, '4': 1, '28': 1}, // Person in Schacht
+          '468' : { '28': 1 }, // Badeunfall
           '476' : { '28': 1, '32': 1}, // Angefahrener Radfahrer
           '518' : { '0': 1 }, // Brennender Baum
           '607' : { '32': 1}, // Verkehrserziehung an Schule
+          '694' : { '0': 2, '4': 1, '32': 1}, // Gülle ausgelaufen
+          '749' : { '28': 1 }, // Allergische Reaktion auf Pollen
+          '727' : { '0': 2 }, // Brennender Picknickkorb
           '733' : { '0': 2 }, // Absicherung Osterfeuer
-          '752' : { '0': 1, '2': 1, '4': 1, '28': 1, '32': 1, '33': 1 }, // Verletzte Person auf Dach
+          '736' : { '0': 1, '32': 1 }, // Schokohase in PKW eingeschlossen
+          '733' : { '0': 1 }, // Geschmolzener Osterhase
+          '749' : { '0': 1, '32': 1}, // E-Call ausgelöst
+          '752' : { '0': 1, '2': 1, '3': 1, '4': 1, '28': 1, '32': 1, '33': 1 }, // Verletzte Person auf Dach
+          '778' : { '0': 1 }, // Brennende Chemietoilette
+          '781' : { '0': 2 }, // Gartenschuppenbrand
       },
+  };
+
+  document.lss_helper.getSetting = (key, def) => {
+      if (!localStorage.getItem('lss_helper_' + key)) {
+          localStorage.setItem('lss_helper_' + key, def ?? 'false');
+      }
+      return JSON.parse(localStorage.getItem('lss_helper_' + key) ?? (def ?? 'false'));
   };
 
   document.lss_helper.init = () => {
@@ -89,7 +139,9 @@
       document.lss_helper.updateLists();
       document.lss_helper.printVehicleList();
       document.lss_helper.printMissions();
-      setTimeout(() => {document.lss_helper.update();}, document.lss_helper.updateInterval);
+      if (document.lss_helper.getSetting('updateInterval', '1000') > 0) {
+          setTimeout(() => {document.lss_helper.update();}, document.lss_helper.getSetting('updateInterval', '1000'));
+      }
   };
 
   document.lss_helper.updateLists = () => {
@@ -114,6 +166,7 @@
       .sort((a,b) => a.status < b.status ? -1 : 1 );
       document.lss_helper.missions = Array.from(document.querySelectorAll(".missionSideBarEntry:not(.mission_deleted)"))
       .map((m) => {
+          const links = Array.from(m.getElementsByTagName('a')).map((l) => l.cloneNode(true));
           return {
               id: m.attributes['id'].value.trim(),
               missionId: m.attributes['mission_id'].value.trim(),
@@ -122,10 +175,12 @@
               participation: m.attributes['data-mission-participation-filter'].value.trim(),
               data: JSON.parse(m.attributes['data-sortable-by'].value.trim()),
               missionType: m.attributes['mission_type_id'].value.trim(),
+              links,
               origin: m,
           }
       })
-      .sort((m1, m2) => m1.data.average_credits - m2.data.average_credits);
+      .sort((m1, m2) => m2.data.average_credits - m1.data.average_credits)
+      .sort((m1, m2) => m1.state < m2.state ? -1 : 0);
   };
 
   document.lss_helper.getHelperContainer = () => {
@@ -177,49 +232,67 @@
       containerAvailable = document.createElement("ul");
       containerAvailable.id = 'lss_helper_vehicle_available';
       containerAvailable.classList = 'col-md-3';
-      main.appendChild(containerAvailable);
+      if (document.lss_helper.getSetting('show_vehicle_call', 'true') || document.lss_helper.getSetting('show_vehicle_available', 'true')) {
+          main.appendChild(containerAvailable);
+      }
+
       containerUnavailable = document.createElement("ul");
       containerUnavailable.id = 'lss_helper_vehicle_unavailable';
       containerUnavailable.classList = 'col-md-3';
-      main.appendChild(containerUnavailable);
+      if (document.lss_helper.getSetting('show_vehicle_unavailable', 'true')) {
+          main.appendChild(containerUnavailable);
+      }
 
       const itemsAvailable = {};
       const itemsUnavailable = {};
       const itemsCall = document.lss_helper.vehicles.filter((v) => v.call);
 
-      document.lss_helper.vehicles
-      .filter((v) => v.available)
-      .forEach((v) => {
-          const idx = v.type;
-          itemsAvailable[idx] = itemsAvailable[idx] || [];
-          itemsAvailable[idx].push(v);
-      });
-      document.lss_helper.vehicles
-      .filter((v) => !v.available)
-      .forEach((v) => {
-          const idx = v.type;
-          itemsUnavailable[idx] = itemsUnavailable[idx] || [];
-          itemsUnavailable[idx].push(v);
-      });
-      Object.values(itemsCall).forEach((i) => {
-          const li = document.createElement('li');
-          li.classList = 'lss_call';
-          li.innerHTML = (document.lss_helper.vehicleTypes[i.type] || i.type) + ' - ' + i.name;
-          li.append(i.link);
-          containerAvailable.append(li)
-      });
-      Object.values(itemsAvailable).forEach((i) => {
-          const li = document.createElement('li');
-          li.classList = 'lss_available';
-          li.innerHTML = i.length + ' ' + (document.lss_helper.vehicleTypes[i[0].type] || (i[0].type + ' - ' + i[0].name));
-          containerAvailable.append(li)
-      });
-      Object.values(itemsUnavailable).forEach((i) => {
-          const li = document.createElement('li');
-          li.classList = 'lss_unavailable';
-          li.innerHTML = i.length + ' ' + (document.lss_helper.vehicleTypes[i[0].type] || (i[0].type + ' - ' + i[0].name));
-          containerUnavailable.append(li)
-      });
+      if (document.lss_helper.getSetting('show_vehicle_available', 'true')) {
+          document.lss_helper.vehicles
+          .filter((v) => v.available)
+          .forEach((v) => {
+              const idx = v.type;
+              itemsAvailable[idx] = itemsAvailable[idx] || [];
+              itemsAvailable[idx].push(v);
+          });
+      }
+
+      if (document.lss_helper.getSetting('show_vehicle_unavailable', 'true')) {
+          document.lss_helper.vehicles
+          .filter((v) => !v.available)
+          .forEach((v) => {
+              const idx = v.type;
+              itemsUnavailable[idx] = itemsUnavailable[idx] || [];
+              itemsUnavailable[idx].push(v);
+          });
+      }
+
+      if (document.lss_helper.getSetting('show_vehicle_call', 'true')) {
+          Object.values(itemsCall).forEach((i) => {
+              const li = document.createElement('li');
+              li.classList = 'lss_call';
+              li.innerHTML = (document.lss_helper.vehicleTypes[i.type] || i.type) + ' - ' + i.name;
+              li.append(i.link);
+              containerAvailable.append(li)
+          });
+      }
+
+      if (Object.keys(itemsAvailable).length) {
+          Object.values(itemsAvailable).forEach((i) => {
+              const li = document.createElement('li');
+              li.classList = 'lss_available';
+              li.innerHTML = i.length + ' ' + (document.lss_helper.vehicleTypes[i[0].type] || (i[0].type + ' - ' + i[0].name));
+              containerAvailable.append(li)
+          });
+      }
+      if (Object.keys(itemsUnavailable).length) {
+          Object.values(itemsUnavailable).forEach((i) => {
+              const li = document.createElement('li');
+              li.classList = 'lss_unavailable';
+              li.innerHTML = i.length + ' ' + (document.lss_helper.vehicleTypes[i[0].type] || (i[0].type + ' - ' + i[0].name));
+              containerUnavailable.append(li)
+          });
+      }
   };
 
   document.lss_helper.printMissions = () => {
@@ -231,38 +304,62 @@
       missionsContainer = document.createElement("ul");
       missionsContainer.id = 'lss_helper_missions';
       missionsContainer.classList = 'col-md-6';
-      main.appendChild(missionsContainer);
+      if (document.lss_helper.getSetting('show_missions', 'true')) {
+          main.appendChild(missionsContainer);
+      }
 
       document.lss_helper.missions
-      .filter((m) => m.state != 'finishing')
+      //.filter((m) => m.state != 'finishing')
       .sort((m1,m2) => m1.state < m2.state ? -1 : 1)
       .forEach((m) => {
           const li = document.createElement('li');
           li.classList = 'state_' + m.state;
+          li.style = 'display:flex; flex-direction:row;justify-content: space-between;';
           missionsContainer.appendChild(li);
 
-          const btn = document.createElement('a');
-          btn.classList= 'btn btn-xs btn-default';
-          btn.innerHTML = '🚒';
-          btn.onclick = () => {document.lss_helper.sendByScene(m, 'lf1')};
+          const btnContainer = document.createElement('span');
+          li.appendChild(btnContainer);
 
-          const btn2 = document.createElement('a');
-          btn2.classList= 'btn btn-xs btn-default';
-          btn2.innerHTML = document.lss_helper.scenes[m.missionType] ? '🚨' : '🚒🚒';
-          btn2.onclick = () => {document.lss_helper.sendByScene(m)};
+          const infoContainer = document.createElement('span');
+          li.appendChild(infoContainer);
 
-          const txt = document.createElement('span');
-          txt.innerHTML = m.data.caption;
+          const age = Math.floor(
+              Math.abs(new Date() - new Date(m.data.created_at * 1000)) / (100 * 60 * 60)
+          ) / 10;
+          infoContainer.innerHTML = [
+              document.lss_helper.getSetting('show_mission_age', 'false') ? age + 'h' : null,
+              document.lss_helper.getSetting('show_mission_credits', 'false') ? m.data.average_credits : null,
+          ].filter((t) => !!t).join(' - ');
+
+          const txt = m.links[0];
+          txt.innerHTML = m.data.caption + ' (' + m.missionType + ')';
+          txt.style = 'margin-left: 4px';
+          li.appendChild(txt);
 
           if (m.state === 'unattended') {
-              if (document.lss_helper.getVehiclesByMission(m, 'lf1')) {
-                  li.appendChild(btn);
-              }
-              if (document.lss_helper.getVehiclesByMission(m)) {
-                  li.appendChild(btn2);
+              if (document.lss_helper.scenes[m.missionType] && document.lss_helper.getVehiclesByMission(m, m.missionType) && document.lss_helper.getSetting('show_mission_type', 'true')) {
+                  const btn2 = document.createElement('a');
+                  btn2.classList= 'btn btn-xs btn-default';
+                  btn2.innerHTML = '🚨';
+                  btn2.onclick = () => {document.lss_helper.sendByScene(m)};
+                  btnContainer.appendChild(btn2);
+              } else {
+                  if (document.lss_helper.getVehiclesByMission(m, 'lf1') && document.lss_helper.getSetting('show_mission_lf1', 'true')) {
+                      const btn = document.createElement('a');
+                      btn.classList= 'btn btn-xs btn-default';
+                      btn.innerHTML = '🚒';
+                      btn.onclick = () => {document.lss_helper.sendByScene(m, 'lf1')};
+                      btnContainer.appendChild(btn);
+                  }
+                  if (document.lss_helper.getVehiclesByMission(m) && document.lss_helper.getSetting('show_mission_lf2', 'true')) {
+                      const btn2 = document.createElement('a');
+                      btn2.classList= 'btn btn-xs btn-default';
+                      btn2.innerHTML = '🚒🚒';
+                      btn2.onclick = () => {document.lss_helper.sendByScene(m)};
+                      btnContainer.appendChild(btn2);
+                  }
               }
           }
-          li.appendChild(txt);
       });
   };
 
