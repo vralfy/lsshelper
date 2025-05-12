dcf3612ac0763aba5a6e91bcc4e71a6ad74186dd
Norman Specht <norman.specht.extern@atruvia.de>
2025-05-12 18:40:42 +0200

    Update version and add new vehicle groups and states
    - Updated the script version to 202505-12-01.
    - Added new vehicle groups: THWLKW, THWMLW, THWBRG, and THWDLE.
    - Added corresponding vehicle states for THW LKW K 9 and THW MLW 5.

--- lsshelper.user.js
+++ lsshelper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Leistellenspiel Helper
 // @namespace    http://tampermonkey.net/
-// @version      202505-09-01
+// @version      202505-12-01
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.leitstellenspiel.de/
