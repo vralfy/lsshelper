b12ec2d6ad75f4adf2e519d6ad89cf956f218366
Norman Specht <norman.specht.extern@atruvia.de>
2025-05-21 17:55:51 +0200

    Add vehicle data fetching to lss_helper
    Finally added the essential vehicle data fetching functions to the lss_helper. This includes fetching vehicle types, groups, states, replacements, and scenes. Now we can actually do something useful with our vehicles instead of just staring at them!

---
+++ lsshelper.update.js
@@ -0,0 +1,5 @@
+document.lss_helper.fetchRemoteFile('vehiclesTypes.js');
+document.lss_helper.fetchRemoteFile('vehicleGroups.js');
+document.lss_helper.fetchRemoteFile('vehicleStates.js');
+document.lss_helper.fetchRemoteFile('vehicleReplacements.js');
+document.lss_helper.fetchRemoteFile('scenes.js');
