651942f59321b0a1fbb9fef0ae2609ed07b5030c
Norman Specht <norman.specht.extern@atruvia.de>
2025-06-12 11:12:54 +0200

    Add new vehicle states and types for THW units
    This commit introduces additional vehicle states and types for various THW (Technisches Hilfswerk) units in the vehicleStates.js and vehiclesTypes.js files. New entries include vehicle states for GKW, MLW, and LKW types, enhancing the game's vehicle management capabilities.
    
    Now you can enjoy a more comprehensive vehicle selection experience!

--- vehicleStates.js
+++ vehicleStates.js
@@ -2,11 +2,17 @@ document.lss_helper.vehicleStatesAvailable = {
   "default": ["1", "2"],
   "call": ["5"],
   "transit": ["7"],
+  "39": ["2"], // THW GKW
   "41": ["2"], // THW MzGW (FGr N)
   "42": ["2"], // THW LKW K 9
   "45": ["2"], // THW MLW 5
   "64": ["2"], // GW-Wasserrettung
+  "65": ["2"], // THW LKW K 7 Lkr
   "93": ["2"], // THW MTW-O
+  "100": ["2"], // THW MLW 4
+  "122": ["2"], // THW LKW 7 Lbw E
+  "123": ["2"], // THW LKW 7 Lbw WP
+  "145": ["2"], // THW FüKomKW
 };
 
 document.lss_helper.statesAvailable = document.lss_helper.vehicleStatesAvailable.default ?? ['1', '2'];
