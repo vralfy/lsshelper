02cd10d79a4acd82b432d08b7b53e94b1ee0a68e
Norman Specht <norman.specht.extern@atruvia.de>
2025-06-20 14:51:24 +0200

    Enhance response units for high-rise fire evacuation scene
    Updated the response units for the high-rise fire evacuation scene (ID: 925) by adding 20 RTWs to improve emergency response capabilities. This adjustment aims to ensure adequate medical support during large-scale evacuations. Because who doesn't want more ambulances when things get heated?

--- scenes.js
+++ scenes.js
@@ -530,7 +530,7 @@ document.lss_helper.scenes = {
   "900" : { "POL": 1 }, // Verdaechtiges Fahrzeug
   "915" : { "LF": 2, "RW": 1, "ELW": 1, "POL": 1, "RTW": 1 }, // Verletzte Person in verlassenem Gebäude
   "916" : { "LF": 6, "ELW": 2, "DLK": 2, "RW": 1, "SW": 1, "GWH": 1, "POL": 4, "RTW": 1 }, // Brand auf alten Frabrikgelände
-  "925" : { "LF": 14, "RW": 1, "ELW": 3, "ELW2": 1, "GWM": 1, "GWA": 2, "DLK": 3, "SW": 1, "POL": 8 }, // Hochhausbrand mit Evakuierung
+  "925" : { "LF": 14, "RW": 1, "ELW": 3, "ELW2": 1, "GWM": 1, "GWA": 2, "DLK": 3, "SW": 1, "POL": 8, "RTW": 20 }, // Hochhausbrand mit Evakuierung
   "926" : { "LF": 4, "ELW": 1 }, // Brandsicherheitswache Rockfestival
   "933" : { "LF": 4, "RW": 1, "ELW": 1, "DLK": 1, "GWG": 1, "GWM": 1, "DEKONP": 1, "POL": 4, "RTW": 2, "NEF": 2 }, // Gefahrgut-Austritt in Paketzentrum
 }
