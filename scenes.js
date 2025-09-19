ecf71eeda78232ca6aea6d3a20276ee37ae0ebb6
Norman Specht <norman.specht.extern@atruvia.de>
2025-09-19 13:58:04 +0200

    Add new scene for clogged drain due to leaves
    This commit introduces a new scene (ID: 676) for a clogged drain caused by leaves, enhancing the game's incident variety. Players can now respond to this common yet critical situation, adding depth to their missions.
    
    Because who doesn't love a good leaf blockage?

--- scenes.js
+++ scenes.js
@@ -507,6 +507,7 @@ document.lss_helper.scenes = {
   "672" : { "RTW": 1 }, // Person auf Laub ausgerutscht
   "674" : { "RTW": 1 }, // Person in Laubhaufen gestürzt
   "675" : { "POL": 1, "RTW": 1 }, // Laubsauger außer Kontrolle
+  "676" : { "RW": 1 }, // Von Laub verstopfter Gulli
   "678" : { "LF": 2, "RW": 1, "RTW": 1 }, // Verkehrsunfall auf nebliger Straße
   "679" : { "LF": 2, "POL": 1, "RW": 1, "RTW": 1 }, // Verkehrsunfall bei Wildwechsel
   "680" : { "LF": 2 }, // Brennender Laubhaufen
