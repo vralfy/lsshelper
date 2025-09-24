383d9b0ab0167b3af66079122d05384468201ff6
Norman Specht <norman.specht.extern@atruvia.de>
2025-09-24 14:11:11 +0200

    Add new scene for burning train station
    This commit introduces a new scene (ID: 606) for a burning train station, enhancing the game's emergency response scenarios. The scene includes various emergency vehicles and personnel to simulate a realistic response to a fire incident at a train station.
    
    Get ready to extinguish the flames and save the day at the station!

--- scenes.js
+++ scenes.js
@@ -457,6 +457,7 @@ document.lss_helper.scenes = {
   "594" : { "LF": 15, "ELW": 2, "ELW2": 1, "DLK": 2, "POL": 6, "GWA": 1, "RW": 1, "THWMTW": 1, "GKW": 1, "THWGWN": 1, "RTW": 1, "NEF": 1 }, //Brand in Schule
   "595" : { "RTW": 1, "NEF": 1 }, // Person von Blitz getroffen
   "596" : { "LF": 3, "RW": 2, "ELW": 1, "POL": 4, "RTW": 2, "NEF": 1 }, // Verkehrsunfall (Personen eingeklemmt)
+  "606" : { "LF": 25, "RW": 2, "ELW": 5, "ELW2": 1, "DLK": 5, "GWM": 3, "GWA": 3, "SW": 1, "POL": 8, "GKW": 3, "THWMTW": 3, "AAOTHWR": 2, "THWGWN": 3, "RTW": 10 }, // Brennender Bahnhof
   "607" : { "POL": 1}, // Verkehrserziehung an Schule
   "608" : { "POL": 1 }, // Abgebrochener Notruf
   "609" : { "LF": 1, "GWH": 1, "RTW": 1 }, // Fensterputzer haengt in Gondel fest
