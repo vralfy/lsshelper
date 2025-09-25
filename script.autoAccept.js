807693c5424c408a3347a7697c0ece32163c3678
Norman Specht <norman.specht.extern@atruvia.de>
2025-09-25 16:14:54 +0200

    Refactor resendGroupsVehicles construction in getResendMissions
    Updated the logic for constructing the resendGroupsVehicles array by filtering out unnecessary vehicle types. Removed hardcoded vehicle entries and streamlined the mapping process to enhance performance and maintainability. Now, only relevant vehicles are included, ensuring a more efficient auto-resend process.
    
    Because who needs extra baggage when you're on a mission?

--- script.autoAccept.js
+++ script.autoAccept.js
@@ -43,11 +43,7 @@ document.lss_helper.getResendMissions = () => {
             });
 
             const resendGroupsVehicles = [
-                ...Object.keys(resendGroupsScene).map((k) => ({ key: k, fok: true })),
-                { key: 'rescue', fok: true },
-                { key: 'rescue2', fok: true },
-                { key: 'water', fok: true },
-                { key: 'firefighter', fok: false },
+                ...Object.keys(resendGroupsScene).filter((k) => ['slf'].indexOf(k) >= 0).map((k) => ({ key: k, fok: true })),
                 { key: 'slf', fok: false },
             ].map((i) => {
                 if (!resendGroupsScene[i.key]) {
