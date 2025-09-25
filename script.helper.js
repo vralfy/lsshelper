1d8f325ee5a7dc92a55a73d8cdf173d4286fa2b5
Norman Specht <webmaster@foopara.de>
2025-09-25 21:36:15 +0200

    Refactor distance calculation logic in getDistance function
    Reorganized the getDistance function to improve readability and maintainability. The logic for calculating the distance based on exact settings has been moved into a conditional block, ensuring clarity in the code structure. This change enhances the overall performance and user experience when determining distances between objects.
    
    Now you can measure distances like a pro, because who doesn't want to be precise in a game about managing resources?

--- script.helper.js
+++ script.helper.js
@@ -23,16 +23,21 @@ document.lss_helper.helper.hash = (str) => {
 };
 
 document.lss_helper.helper.getDistance = (obj1, obj2) => {
-  const lat = (obj1.lat ?? 0) - (obj2.lat ?? 0);
-  const lng = (obj1.lng ?? 0) - (obj2.lng ?? 0);
-  // return Math.sqrt(lat * lat + lng * lng);
-  const radius = 6371; // Radius of the Earth in km
-  const radLat1 = obj1.lat * Math.PI / 180;
-  const radLat2 = obj2.lat * Math.PI / 180;
-  const dLat = (obj1.lat - obj2.lat) * Math.PI / 180;
-  const dLon = (obj1.lng - obj2.lng) * Math.PI / 180;
-  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(radLat1) * Math.cos(radLat1) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  return radius * c; // Distance in km
+  if (document.lss_helper.getSetting('exactDistance')) {
+    const lat = (obj1.lat ?? 0) - (obj2.lat ?? 0);
+    const lng = (obj1.lng ?? 0) - (obj2.lng ?? 0);
+    // return Math.sqrt(lat * lat + lng * lng);
+    const radius = 6371; // Radius of the Earth in km
+    const radLat1 = obj1.lat * Math.PI / 180;
+    const radLat2 = obj2.lat * Math.PI / 180;
+    const dLat = (obj1.lat - obj2.lat) * Math.PI / 180;
+    const dLon = (obj1.lng - obj2.lng) * Math.PI / 180;
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(radLat1) * Math.cos(radLat1) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return radius * c; // Distance in km
+  }
+  diffLat = Math.abs((obj1.lat ?? 0) - (obj2.lat ?? 0));
+  diffLng = Math.abs((obj1.lng ?? 0) - (obj2.lng ?? 0));
+  return Math.sqrt(diffLat * diffLat + diffLng * diffLng);
 }
