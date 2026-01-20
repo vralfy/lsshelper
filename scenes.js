02bb9439b65b93829ef3513e847b9dea505c950a
Norman Specht <norman.specht.extern@atruvia.de>
2026-01-20 14:26:49 +0100

    Add new scene for state visit security
    This commit introduces a new scene (ID: 563) for securing a state visit, enhancing the emergency response scenarios available in the script. The scene includes specific resource allocations for various units, ensuring a comprehensive response strategy.
    
    Now you can secure your state visits like a pro—because nothing says "welcome" like a well-coordinated emergency response!

--- scenes.js
+++ scenes.js
@@ -466,6 +466,7 @@ document.lss_helper.scenes = {
   "558" : { "LF": 8, "RW": 1, "DLK": 1, "ELW": 2, "POL": 4, "RTW": 10, "NEF": 1}, // Brennende Bühne
   "561" : { "RTW": 1 }, // Fremdkörper in Wunde
   "562" : { "LF": 5, "RW": 2, "GWO": 1, "ELW": 1, "POL": 4, "RTW": 2, "NEF": 1 }, // Verkehrsunfall in Tunnel
+  "563" : { "LF": 2, "POL": 12, "GRUKW": 9, "BEFKW": 3, "FUEKW": 1, "RTW": 3, "GWSAN": 1 }, // Absicherung Staatsbesuch
   "564" : { "FUEKW": 1, "MEK": 4, "POL": 1 }, // Geplante Festnahme
   "565" : { "FUEKW": 1, "MEK": 4, "POL": 4 }, // Zugriff bei Geldübergabe
   "566" : { "POL": 1 }, // Drogenhandel aufgeflogen
