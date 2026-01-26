51856ec7e591a66732eb6e17e3ee50264e30ee18
Norman Specht <webmaster@foopara.de>
2026-01-26 21:44:17 +0100

    Add education management structure for emergency services
    This commit introduces a new JavaScript object to manage education-related data for various emergency services, including Feuerwehr, Rettungsdienst, Polizei, and THW. The structure is designed to facilitate future enhancements in vehicle education checks and related functionalities.
    
    Now you can finally educate your vehicles, because who wouldn't want a well-trained emergency response team?

---
+++ script.education.js
@@ -0,0 +1,35 @@
+// https://www.leitstellenspiel.de/buildings/{{schoolId}}
+// https://www.leitstellenspiel.de/buildings/{{schoolId}}/schoolingEducationCheck?education={{educationId}}&only_building_id={{buildingId}}
+document.lss_helper.education = {
+  '1': { // Feuerwehr
+    '0': { // Feuerwehrwache
+
+    },
+  },
+  '3': { // Rettungsdienst
+    '2': { // Rettungswache
+      '1': 'LNA'
+    },
+    '5': { // Rettungshubschrauber
+    },
+    '12': { // SEG
+    },
+    '15': { // DLRG
+    },
+  },
+  '8': { // Polizei
+    '6': { // Polizeiwache
+    },
+    '11': { // BePo
+    },
+    '13': { // BePo Heli
+    },
+    '17': { // BePo Sondereinheiten
+    },
+  },
+  '10': { // THW
+    '9': { // THW
+
+    },
+  },
+};
