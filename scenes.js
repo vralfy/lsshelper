d4e85944ae91349c207e6518cca1b6eb787234e2
Norman Specht <norman.specht.extern@atruvia.de>
2026-01-30 13:43:31 +0100

    Add new scenes for major fire incidents and toxic chocolates
    This commit introduces two new scenes to enhance gameplay:
    1. Scene 919 for a large fire caused by fallen candles, requiring significant resources.
    2. Scene 920 for a case of poisoned chocolates, involving police and medical response.
    
    Get ready to extinguish flames and save the day, one chocolate at a time!

--- scenes.js
+++ scenes.js
@@ -721,6 +721,8 @@ document.lss_helper.scenes = {
   "915" : { "LF": 2, "RW": 1, "ELW": 1, "POL": 1, "RTW": 1 }, // Verletzte Person in verlassenem Gebäude
   "916" : { "LF": 6, "ELW": 2, "DLK": 2, "RW": 1, "SW": 1, "GWH": 1, "POL": 4, "RTW": 1 }, // Brand auf alten Frabrikgelände
   "918" : { "LF": 2, "DLK": 1 }, // Unterstützung bei Feuerwehr-Heiratsantrag
+  "919" : { "LF": 10, "ELW": 3, "ELW2": 1, "DLK": 3, "RW": 2, "POL": 5, "RTW": 5, "NEF": 1 }, // Großfeuer durch umgefallene Kerzen
+  "920" : { "POL": 1, "RTW": 1, "NEF": 1 }, // Vergiftete Pralinen
   "925" : { "LF": 14, "RW": 1, "ELW": 3, "ELW2": 1, "GWM": 1, "GWA": 2, "DLK": 3, "SW": 1, "POL": 8, "RTW": 20 }, // Hochhausbrand mit Evakuierung
   "926" : { "LF": 4, "ELW": 1 }, // Brandsicherheitswache Rockfestival
   "930" : { "LF": 6, "RW": 1, "DLK": 1, "ELW": 2, "GWA": 1, "SW": 1, "POL": 4, "RTW": 1 }, // Feuer im Zoo
