3f2963f5e7eba9896420172a6050bb5e5ccf50d3
Norman Specht <norman.specht.extern@atruvia.de>
2026-01-30 13:45:20 +0100

    Add new scenes for marriage proposal shock and Valentine's restaurant fire
    This commit introduces two new scenes to enhance gameplay: one depicting a shock from a marriage proposal and another featuring a fire caused by Valentine's decorations in a restaurant. These additions aim to diversify the scenarios players encounter, adding depth and excitement to the game.
    
    Who knew love could be so flammable?

--- scenes.js
+++ scenes.js
@@ -498,6 +498,8 @@ document.lss_helper.scenes = {
   "595" : { "RTW": 1, "NEF": 1 }, // Person von Blitz getroffen
   "596" : { "LF": 3, "RW": 2, "ELW": 1, "POL": 4, "RTW": 2, "NEF": 1 }, // Verkehrsunfall (Personen eingeklemmt)
   "598" : { "RTW": 1 }, // Schmetterlinge im Bauch
+  "599" : { "RTW": 1 }, // Schock durch Heiratsantrag
+  "601" : { "LF": 5, "DLK": 1, "ELW": 1 }, // Valentinsdekoration in Restaurant brennt
   "602" : { "LF": 1, "RTW": 2 }, // Feuer durch Abendessen bei Kerzenlicht
   "606" : { "LF": 25, "RW": 2, "ELW": 5, "ELW2": 1, "DLK": 5, "GWM": 3, "GWA": 3, "SW": 1, "POL": 8, "GKW": 3, "THWMTW": 3, "AAOTHWR": 2, "THWGWN": 3, "RTW": 10 }, // Brennender Bahnhof
   "607" : { "POL": 1}, // Verkehrserziehung an Schule
