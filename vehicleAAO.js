dd05758d27b7106bec3bacb6536106c76b2239ff
Norman Specht <norman.specht.extern@atruvia.de>
2026-03-18 10:40:16 +0100

    Refactor vehicleAAO entries for clarity and consistency
    Updated the vehicleAAO object to ensure all vehicle types are correctly defined and organized. Adjusted the entries for AAOTHWNEA200 and added AAOTHWBRB for better vehicle classification. Removed commented-out lines for a cleaner codebase and improved readability.
    
    Now you can send in the big guns when the situation gets hot—because who doesn't love a good fire drill?

--- vehicleAAO.js
+++ vehicleAAO.js
@@ -5,20 +5,27 @@ document.lss_helper.vehicleAAO = {
   "AAOTHWFUE": { 144: 1, 145: 1, 146: 1, 147: 1, 148: 1 },
   "AAOFFNEA": { "NEA50": 1 },
   "AAOTHWNEA50": { "THWGWN": 1, "NEA50": 1 },
-  "AAOTHWNEA200": { "NEA200": 1 }, // 122: 1,
+  "AAOTHWNEA200": { 122: 1, "NEA200": 1 },
   "AAOTHWR": { 43: 1 }, //, 42: 1 },
   "AAOTHWWP": { 100: 1, 101: 1, 102: 1, 123: 1 },
+  "AAOTHWBRB": { 181: 1, 182: 1, 183: 1 },
   "AAOSEGBT": { 130: 1, 132: 1, 133: 1 },
   "AAOTESI": { 173: 1, 174: 1 },
 };
 
+// These are reduced entries for the AAO, which are used to determine which vehicles can be sent to an incident.
+// The full list of vehicles and their corresponding types is defined in vehicleResend.js and vehiclesTypes.js.
+// The AAO entries here are used to group certain vehicle types together for specific incident types.
+// For example, "AAOTHWFUE" includes all the THW vehicles that are classified as "FüKW", "FüKomKW", "Anh FüLa", "FmKW", and "MTW-FGr K".
+// This allows the game to know that any of these vehicles can be sent to an incident that requires a "FüKW".
 document.lss_helper.vehicleAAO = {
   ...document.lss_helper.vehicleAAO,
-  "AAOTHWDLE": { 44: 1 }, // 45: 1
-  "AAOTHWDOG": { 92: 1 }, // 93: 1
-  "AAOTHWFUE": { 144: 1, 146: 1, 147: 1, 148: 1 }, // 145: 1
-  "AAOTHWNEA50": { "NEA50": 1 }, // "THWGWN": 1
-  "AAOTHWNEA200": { "NEA200": 1 }, // 122: 1
-  "AAOTHWWP": { 101: 1, 102: 1 }, // 100: 1, 123: 1
-  "AAOSEGBT": { 130: 1, 132: 1 }, // 133: 1
+  "AAOTHWDLE": { 44: 1 },
+  "AAOTHWDOG": { 92: 1 },
+  "AAOTHWFUE": { 144: 1, 146: 1, 147: 1, 148: 1 },
+  "AAOTHWNEA50": { "NEA50": 1 },
+  "AAOTHWNEA200": { "NEA200": 1 },
+  "AAOTHWWP": { 101: 1, 102: 1 },
+  "AAOTHWBRB": { 182: 1, 183: 1 },
+  "AAOSEGBT": { 130: 1, 132: 1 },
 };
