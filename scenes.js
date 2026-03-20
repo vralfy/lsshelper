06f344ebc3e53f9642f9883a4c87fad419e06570
Norman Specht <norman.specht.extern@atruvia.de>
2026-03-20 15:15:25 +0100

    Add new scene for Easter-themed incident involving a boat
    This commit introduces a new scene (ID: 442) for the Easter-themed incidents, specifically depicting an egg in water. This addition enhances the variety of scenarios players can encounter during the Easter events.
    
    Because nothing says Easter like a floating egg adventure!

--- scenes.js
+++ scenes.js
@@ -397,6 +397,7 @@ document.lss_helper.scenes = {
   "439" : { "LF": 18, "RW": 2, "SW": 1, "DLK": 3, "FWK": 2, "ELW": 5, "ELW2": 1, "POL": 10, "THWMTW": 2, "THWGWN": 2, "AAOTHWR": 2, "GKW": 2, "RTW": 1, "NEF": 1}, // Einsturz Parkhaus
   "440" : { "RTW": 1, "NEF": 1 }, // Akute Atemnot
   "441" : { "LF": 3, "ELW": 1, "RW": 1 }, // Brennende Waldhütte
+  "442" : { "BOAT": 1 }, // Osterei in Wasser
   "443" : { "LF": 2, "RW": 1, "ELW": 1, "RTW": 2, "FWK": 1 }, // Osterei-LKW gestürzt
   "445" : { "POL": 1 }, // Ei rollt in Schlangenlinien
   "446" : { "LF": 1, "ELW": 1, "POL": 1, "GWH": 1 }, // Osterhase auf Kran
