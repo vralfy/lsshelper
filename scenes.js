3f4a8dd01e9aafc0cc1869186b49bfab41b0fd3f
Norman Specht <norman.specht.extern@atruvia.de>
2026-06-16 14:09:58 +0200

    Fix vehicle type in scene configuration for LKW recovery operation
    Updated the vehicle type from 'TKWMTW' to 'THWMTW' in the scene configuration for incident 1002, which involves the recovery of a truck on the highway. This change ensures the correct vehicle is utilized for the operation.
    
    Because nothing says teamwork like a well-coordinated truck recovery!

--- scenes.js
+++ scenes.js
@@ -855,7 +855,7 @@ document.lss_helper.scenes = {
   "999" : { "POLAP": 6 }, // Fan-Korso auf Autobahn
   "1000" : { "POLAP": 1 }, // Rettungsgasse kontrollieren
   "1001" : { "POLAP": 2, "LF": 2, "GWO": 1 }, // Ölspur auf Autobahn
-  "1002" : { "POLAP": 1, "GKW": 1, "TKWMTW": 1, "THWGWN": 1, "LF": 1, "RW": 1, "AAOTHWR": 1 }, // Ladungsbergung eines LKW auf Autobahn
+  "1002" : { "POLAP": 1, "GKW": 1, "THWMTW": 1, "THWGWN": 1, "LF": 1, "RW": 1, "AAOTHWR": 1 }, // Ladungsbergung eines LKW auf Autobahn
   "1003" : { "POLAP": 1 }, // Platzverweis missachtet
   "1004" : { "POL": 2, "MEK": 4, "FUEKW": 1, "RTW": 1, "NEF": 1 }, // Ausrastenden Trainer unter Kontrolle bringen
   "1005" : { "RTW": 1 }, // Zusammenbruch durch Niederlage
