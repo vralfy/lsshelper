f4afb42ece6da0873107cf4b172d80e8ed4846e8
Norman Specht <norman.specht.extern@atruvia.de>
2026-06-30 08:48:39 +0200

    Add new scene for Badestellenüberwachung
    This commit introduces a new scene configuration for monitoring bathing areas, identified by the scene ID "547". The configuration includes the deployment of one boat unit to enhance safety at these locations.
    
    Because who wouldn't want to keep an eye on the water while enjoying the sun?

--- scenes.js
+++ scenes.js
@@ -477,6 +477,7 @@ document.lss_helper.scenes = {
   "541" : { "GKW": 1, "THWMTW": 1, "AAOTHWR": 1, "POL": 2, "THWGWSB": 1, "DOG": 1 }, // Erdrutsch
   "544" : { "LF": 3, "RW": 1, "ELW": 1, "GWM": 1, "GWG": 1, "DEKONP": 1, "POL": 2, "RTW": 2, "NEF": 1 }, // Säureangriff auf Hauswand
   "545" : { "LF": 14, "RW": 2, "DLK": 3, "ELW": 2, "GWA": 1, "SW": 1, "POL": 4, "GKW": 1, "AAOTHWR": 1, "THWGWN": 1, "THWMTW": 1, "RTW": 2 }, // Brand bei Holzverarbeiter
+  "547" : { "BOAT": 1 }, // Badestellenüberwachung
   "548" : { "LF": 1 }, // Grillbrand
   "549" : { "MEK": 4 }, // Absicherung Geldtransport
   "550" : { "LF": 5, "RW": 3, "ELW": 1, "GWO": 1, "FWK": 1, "GKW": 1, "POL": 4, "RTW": 2, "NEF": 1 }, // LKW auf PKW gestürzt
