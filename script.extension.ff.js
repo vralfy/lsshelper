4c9c8e3d6e3bc8a7b6ab72a5ad0ce46fbd7d0665
Norman Specht <webmaster@foopara.de>
2026-07-19 19:08:11 +0200

    Refactor fire brigade extension management
    This commit refactors the fire brigade extension management by separating the standard extensions into their own object. The 'ffstandard' object now contains the 'NEA' and 'Luefter' extensions, while the 'ff' object extends 'ffstandard' and includes the 'NEA200' extension. This change improves code organization and clarity, making it easier to manage and extend fire brigade functionalities.
    
    Because every firefighter knows that a well-organized toolbox is the key to saving the day!

--- script.extension.ff.js
+++ script.extension.ff.js
@@ -1,8 +1,11 @@
 document.lss_helper.extensions = document.lss_helper.extensions || {};
-document.lss_helper.extensions.ff = {
+document.lss_helper.extensions.ffstandard = {
   'NEA': 14,
-  'NEA200': 15,
   'Luefter': 16,
+};
+document.lss_helper.extensions.ff = {
+  ...document.lss_helper.extensions.ffstandard,
+  'NEA200': 15,
 };
 
 Object.entries(document.lss_helper.extensions.ff).forEach(([key, value]) => {
@@ -12,6 +15,13 @@ Object.entries(document.lss_helper.extensions.ff).forEach(([key, value]) => {
 
 document.lss_helper.doAllFFExtensions = (start, end) => {
   Object.values(document.lss_helper.extensions.ff).forEach((value) => {
+    document.lss_helper.buyFirebrigadeExtension(value, start, end);
+    document.lss_helper.makeFirebrigadeExtensionReady(value, start, end);
+  });
+};
+
+document.lss_helper.doStandardFFExtensions = (start, end) => {
+  Object.values(document.lss_helper.extensions.ffstandard).forEach((value) => {
     document.lss_helper.buyFirebrigadeExtension(value, start, end);
     document.lss_helper.makeFirebrigadeExtensionReady(value, start, end);
   });
