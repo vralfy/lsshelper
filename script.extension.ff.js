e7f96a704e6630724d8c2cda1819942a2ae30732
Norman Specht <webmaster@foopara.de>
2026-07-19 21:35:10 +0200

    Add additional fire brigade extension definitions
    This commit expands the fire brigade extension management by adding new entries to both the ffstandard and ff extension objects. The following extensions have been added: 'RTW', 'WASSER', 'AIRPORT', 'WERK', 'DRONE', 'Bt', 'Train', and various 'AB' and 'Anh' entries. This enhancement improves the flexibility and functionality of the extension management system, allowing for better vehicle categorization and management.
    
    Because every fire brigade deserves a full toolbox!

--- script.extension.ff.js
+++ script.extension.ff.js
@@ -2,10 +2,36 @@ document.lss_helper.extensions = document.lss_helper.extensions || {};
 document.lss_helper.extensions.ffstandard = {
   'NEA': 14,
   'Luefter': 16,
+  'AB1': 1,
+  'Anh1': 20,
 };
 document.lss_helper.extensions.ff = {
   ...document.lss_helper.extensions.ffstandard,
+  'RTW': 0,
+  'WASSER': 6,
+  'AIRPORT': 8,
+  'WERK': 13,
   'NEA200': 15,
+  'DRONE': 18,
+  'Bt': 19,
+  'Train': 25,
+  //'Big': 9,
+  'AB2': 2,
+  'AB3': 3,
+  'AB4': 4,
+  'AB5': 5,
+  'AB6': 7,
+  'AB7': 10,
+  'AB8': 11,
+  'AB9': 12,
+  'AB10': 17,
+  'AB11': 26,
+  'AB12': 27,
+  'AB13': 28,
+  'Anh2': 21,
+  'Anh3': 22,
+  'Anh4': 23,
+  'Anh5': 24,
 };
 
 Object.entries(document.lss_helper.extensions.ff).forEach(([key, value]) => {
