e80f8529b8aab22ec47a0dc1e9ad999614eaa6e3
Norman Specht <norman.specht.extern@atruvia.de>
2026-08-03 11:13:29 +0200

    Add scene 1012 for Einbruch auf Flughafengelände
    This commit introduces a new scene (ID: 1012) with police requirements for an incident involving a break-in at the airport premises. The addition enhances the game's scenario variety and provides players with more engaging missions.
    
    Remember, even the airport needs a little extra security sometimes!

--- scenes.js
+++ scenes.js
@@ -868,5 +868,6 @@ document.lss_helper.scenes = {
   "1009" : { "LF": 5, "RW": 1, "ELW": 1, "DLK": 1, "GKW": 2, "THWGWN": 2, "THWMTW": 1, "POL": 6, "RTW": 10, "NEF": 1 }, // Eingestürzte VIP-Lounge
   "1010" : { "SEGELW": 1, "RTW": 4, "GWSAN": 1, "LF": 2, "POL": 1 }, // Absicherung Volksfest (klein)
   "1011" : { "POL": 10, "GRUKW": 6, "BEFKW": 2, "DOGPOL": 1, "RTW": 2 }, // Geburtstagsfeier außer Kontrolle
+  "1012" : { "POL": 3 }, // Einbruch auf Flughafengelände
 }
 // https://www.leitstellenspiel.de/einsaetze/0
