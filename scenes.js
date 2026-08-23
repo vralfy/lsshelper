b35f683aa24bd36323737197bd8eba5887d78f63
Norman Specht <webmaster@foopara.de>
2026-08-23 17:01:51 +0200

    Add scene for dog attack with updated vehicle requirements
    This commit introduces a new scene (ID: 1017) for a dog attack, specifying the necessary vehicles: 2 police units, 1 GWT, and 1 RTW. This addition enhances the game's realism and response scenarios.
    
    Get ready to unleash the hounds!

--- scenes.js
+++ scenes.js
@@ -870,6 +870,7 @@ document.lss_helper.scenes = {
   "1010" : { "SEGELW": 1, "RTW": 4, "GWSAN": 1, "LF": 2, "POL": 1 }, // Absicherung Volksfest (klein)
   "1011" : { "POL": 10, "GRUKW": 6, "BEFKW": 2, "DOGPOL": 1, "RTW": 2 }, // Geburtstagsfeier außer Kontrolle
   "1012" : { "POL": 3 }, // Einbruch auf Flughafengelände
+  "1017" : { "POL": 2, "GWT": 1, "RTW": 1 }, // Hundeangriff
   "1019" : { "POL": 1, "GWT": 1, "RTW": 1, "NEF": 1 }, // Reitunfall mit Pkw
   "1026" : { "LF": 6, "DLK": 2, "SW": 1, "GWA": 1, "ELW": 2, "POL": 2, "GWT": 1 }, // Brennender Stall - Tierrettung
   "1027" : { "LF": 15, "DLK": 1, "ELW": 3, "ELW2": 1, "GWA": 1, "GWG": 1, "GWM": 2, "SW": 1, "DEKONP": 1, "POL": 2, "GKW": 1, "THWGWN": 1, "THWMTW": 1, "DROHNE": 1, "NEA50": 1, "RTW": 1, "GWW": 1, "ULF": 1, "TELE": 1, "TURBO": 1 }, // Brand eines Großbatteriespeichers
