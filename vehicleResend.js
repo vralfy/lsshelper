73dbeef4df2da72a666a83b46a14a1270b2754f7
Norman Specht <norman.specht.extern@atruvia.de>
2026-09-07 16:17:15 +0200

    Add new vehicle type for mountain rescue operations
    This commit introduces a new vehicle type 'Bergrettungsfahrzeug' (BERG) to the vehicle resend mappings. This addition enhances the game's vehicle management for mountain rescue scenarios, improving gameplay and operational efficiency.
    
    Because every mountain needs its hero on wheels!

--- vehicleResend.js
+++ vehicleResend.js
@@ -18,6 +18,7 @@ document.lss_helper.vehicleResend = {
   'GW-Öl': 'GWO',
   'GW-Tierrettung': 'GWT',
   'GW-Bergrettung': 'GWB',
+  'Bergrettungsfahrzeug': 'BERG',
   'Dekon-P': 'DEKONP',
   'Lüfter': 'LUEFTER',
 
